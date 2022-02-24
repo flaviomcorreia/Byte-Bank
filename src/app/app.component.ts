baa2db46ff698bdb33e29e3b22b681a831a5d534
Flávio Monteiro <flaviomcorreia17@gmail.com>
2022-02-24 16:24:26 -0300

    Criação do novo Component
    Remoção do Código da Carol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @Output() valoresComErro = new EventEmitter<string>();
+
   title = 'byte_bank';
   destino = 0;
   valor = 0;
