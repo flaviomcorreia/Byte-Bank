3671ae0ee3be8013e49e5c9a2a5f8b9b47e41df8
Flávio Monteiro <flaviomcorreia17@gmail.com>
2022-02-25 13:27:34 -0300

    Component Filho
    Eu tô básicamente pegando a informação de um componente pai, e passando para um componente filho e exibindo, porém, nesse processo eu acabo sobrescrevendo o resultando anterior mostrado na tela, o que não é interessante quanto se trata de um extrato.

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
-  transferencia: any;
+  @Input() transferencia: any;
 
   constructor() { }
 
